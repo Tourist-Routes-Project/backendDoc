dfb5e9c6435b2f3a7df0b3e5c9f700116f8b77e3
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2025-02-16 19:44:26 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'SoundWaysDoc',
-			social: {
-				github: 'https://github.com/Tourist-Routes-Project',
-			},
-			sidebar: [
-				{
-					label: 'Backend',
-					items: [
-						{ label: 'RutaCheckpoint', slug: 'backend/ruta-checkpoint' },
-						{ label: 'Audioguias', slug: 'backend/audioguia' },
-						{ label: 'Comentarios', slug: 'backend/comentarios' },
-					],
-				},
-				{
-					label: 'Frontend',
-					autogenerate: { directory: 'frontend' },
-				},
-			],
-		}),
-	],
+  site: "https://tourist-routes-project.github.io/backendDoc/",
+  base: "/backendDoc/",
+  integrations: [
+    starlight({
+      title: "SoundWaysDoc",
+      social: {
+        github: "https://github.com/Tourist-Routes-Project",
+      },
+      sidebar: [
+        {
+          label: "Backend",
+          items: [
+            { label: "RutaCheckpoint", slug: "backend/ruta-checkpoint" },
+            { label: "Audioguias", slug: "backend/audioguia" },
+            { label: "Comentarios", slug: "backend/comentarios" },
+          ],
+        },
+        {
+          label: "Frontend",
+          autogenerate: { directory: "frontend" },
+        },
+      ],
+    }),
+  ],
 });
